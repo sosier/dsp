56d60981346a04a6e6932f6454bc35aa6d426e3d
Sean Osier <sean@seanosier.com>
2015-09-02 00:50:51 -0500

    Updated Q5 .py file

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,30 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+import datetime as dt
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+# a)
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
+dateFormat = "%m-%d-%Y"
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+start = dt.datetime.strptime(date_start, dateFormat)
+stop = dt.datetime.strptime(date_stop, dateFormat)
+print(stop - start)  # Prints 937 days
+
+# b)
+date_start = '12312013'
+date_stop = '05282015'
+dateFormat = "%m%d%Y"
+
+start = dt.datetime.strptime(date_start, dateFormat)
+stop = dt.datetime.strptime(date_stop, dateFormat)
+print(stop - start)  # Prints 513 days
+
+# c)
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+dateFormat = "%d-%b-%Y"
+
+start = dt.datetime.strptime(date_start, dateFormat)
+stop = dt.datetime.strptime(date_stop, dateFormat)
+print(stop - start)  # Prints 7850 days
